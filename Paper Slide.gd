cf859db4b65b791e381e6aed58d43450a09d58ad
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-10-24 13:01:17 +1300

    changes

---
+++ Paper Slide.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+
+func _input(event):
+	if event.is_action_pressed("Start"):
+		play("Paper Slide")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
